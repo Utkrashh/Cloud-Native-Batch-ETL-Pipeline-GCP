76fb30868befc744049010aa63f7daf140936689
Utkrashh <utkrashh@gmail.com>
2025-04-18 14:32:54 +0530

    Refactor Airflow setup: remove deprecated .env.example and docker-compose.yaml, add new configurations for improved environment management

--- airflow/dags/coingecko_batch_dag.py
+++ airflow/dags/coingecko_batch_dag.py
@@ -30,7 +30,7 @@ dag = DAG(
     "coingecko_etl_dag",
     default_args=default_args,
     description="A DAG for ETL pipeline to load real-time CoinGecko data into BigQuery",
-    schedule_interval="@daily",
+    schedule_interval="*/2 * * * *",
     start_date=days_ago(1),
     catchup=False,
 )
